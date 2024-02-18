a0ce94481871c11692b4a7b725d8bc3ba819fb55
gaeZZang <128779897+gaeZZang@users.noreply.github.com>
2024-02-19 02:33:31 +0900

    오류수정

--- Assets/Script/Dahee/InfoBook/Drag.cs
+++ Assets/Script/Dahee/InfoBook/Drag.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class Drag : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     private RectTransform rectTransform;
     private Vector2 pointerOffset; 
+    private bool isDragging = false; 
 
     void Start()
     {
@@ -14,15 +15,29 @@ public class Drag : MonoBehaviour, IPointerDownHandler, IDragHandler
 
     public void OnPointerDown(PointerEventData eventData)
     {
+      
         transform.SetAsLastSibling();
 
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out pointerOffset);
+      
+        if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, eventData.position, eventData.pressEventCamera))
+        {
+            isDragging = true;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (!isDragging)
+            return;
+
         Vector2 mousePosition = eventData.position;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform.parent as RectTransform, mousePosition, eventData.pressEventCamera, out Vector2 localPoint);
         rectTransform.localPosition = localPoint - pointerOffset;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
     }
 }
